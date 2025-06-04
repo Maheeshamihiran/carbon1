c5e0eb3adc3a51a20395d26f443daaeab936bab9
Maheeshamihiran <maheeshamihiran2021@gmail.com>
2025-06-04 23:01:42 +0530

    Update styles for various components: change background colors, adjust margins, and rename navigation class

--- src/component/LoginPage/LoginPage.jsx
+++ src/component/LoginPage/LoginPage.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-wrapper">
-        <nav className="web-app-nav-bar">
+        <nav className="web-app-nav-bar1">
           <div className="containerlogin">
             <div className="logo">
               <img
